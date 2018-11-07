1bd625e6b9d09d9b85f76b96c7a2301bd0b65973
Jerry Wu <wuj828@gmail.com>
2018-11-07 13:53:40 -0800

    Added logic to set hours, minutes, seconds, then update rotation styling on interval.

--- clock.js
+++ clock.js
@@ -1,1 +1,16 @@
-let time = Date.now();
+const hourHand = document.querySelector('.hour-hand');
+const minuteHand = document.querySelector('.minute-hand');
+const secondHand = document.querySelector('.second-hand');
+
+const updateClock = () => {
+  const time = new Date();
+  const hours = (time.getHours() / 24) * 360;
+  const minutes = (time.getMinutes() / 60) * 360;
+  const seconds = (time.getSeconds() / 60) * 360;
+  console.log(time.getSeconds())
+  hourHand.style.transform = `rotate(${hours + 90}deg)`;
+  minuteHand.style.transform = `rotate(${minutes + 90}deg)`;
+  secondHand.style.transform = `rotate(${seconds + 90}deg)`;
+}
+
+setInterval(updateClock, 1000)
